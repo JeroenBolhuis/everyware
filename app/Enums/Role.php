9f206514553857b8b233114dd3504bc1357ac576
Reigen073 <129341199+Reigen073@users.noreply.github.com>
2026-05-05 13:02:12 +0200

    Description added for what every role does
    feat(users): implement expandable role descriptions in user create/edit forms and in role

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -16,4 +16,12 @@ enum Role: string
             self::User => __('Gebruiker'),
         };
     }
+    public function description(): string
+    {
+        return match ($this) {
+            self::Admin => __('Kan gebruikers bekijken, aanmaken, bewerken en verwijderen. Kan ook rollen toewijzen. Kan ook alles wat een LIC-medewerker kan.'),
+            self::LicEmployee => __('LIC-medewerker rol. Kan gebruikers enquete aanmaken, bekijken, bewerken en verwijderen. Ze kunnen niet alle informatie zien van gebruikers en kunnen ook geen rollen toewijzen.'),
+            self::User => __('Standaard gebruiker. Heeft geen toegang tot gebruikersbeheer, maar kan enquete invullen en informatie terug trekken.'),
+        };
+    }
 }
